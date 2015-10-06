1c3aefb95a2785cfcb1169bf8037c2814165528b
GIT BEST-HOSTING <podpora@best-hosting.cz>
2015-10-06 13:47:47 +0200

    sample MySQLi and REST adapters

---
+++ src/SynKnot/Application/Adapters/SampleMySQLiDNSRecordsAdapter.php
@@ -0,0 +1,38 @@
+<?php 
+namespace SynKnot\Application\Adapters;
+
+use SynKnot\Exception\SynKnotException;
+
+class SampleMySQLiDNSRecordsAdapter extends AbstractDataAdapter{
+	public function getData() {
+		$data = array();
+
+		$connection = mysqli_connect($this->config['server'], 
+									$this->config['login'], 
+									$this->config['password'], 
+									$this->config['database']);
+		
+		if(!$connection){
+			throw new SynKnotException(mysqli_error($connection));
+		}
+		
+		$result = $connection->query('SELECT CONCAT(d.name, ".", d.tld) ' .
+			'AS domainName, d.dnssec, r.name, r.type, r.content, r.ttl, r.priority ' .
+			'FROM dns_record r, domain d, item i ' .
+			'WHERE d.id = r.domain_id AND r.status = "active" ' .
+			'AND i.id = d.id ' .
+			'AND i.status IN ("waiting", "active") ' .
+			'ORDER BY d.id, r.type, r.name, r.content;');
+
+		if(!$result){
+			throw new SynKnotException('Bad result: ' . mysqli_error($connection));
+		}
+		
+		while ($row = mysqli_fetch_assoc($result)) {
+			//domain name
+			$data[$row['domainName']][] = $row;
+		}
+		
+		return $data;
+	}
+}
